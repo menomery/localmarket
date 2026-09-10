a3cf052358a2742ef7ad1c63bcd1fa3b6f22a880
menomery <menomery.4@gmail.com>
2026-09-10 16:27:20 +0300

    Fix typo in forgot password instructions

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -37,7 +37,7 @@ export default function ForgotPassword() {
             Reset Password
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
-            Enter your email address and we'll send you a link to reset your password.
+            Enter your email address and we will send you a link to reset your password.
           </p>
         </div>
         
