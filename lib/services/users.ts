86f679347bc5d70419a1634726277a9aef6d89bb
menomery <menomery.4@gmail.com>
2026-09-10 18:47:56 +0300

    Add files via upload

--- lib/services/users.ts
+++ lib/services/users.ts
@@ -1,4 +1,4 @@
-import { doc, getDoc, setDoc, serverTimestamp } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "../firebase/config";
 import type { UserProfile, UserRole } from "@/types/user";
 
